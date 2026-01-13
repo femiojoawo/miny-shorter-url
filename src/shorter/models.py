abe44f37484be24ec4549a25f0263d62d5c7d50f
femi <femiojoawo@3gmail.com>
2026-01-13 18:16:09 +0000

    creation de la base de donnees

--- src/shorter/models.py
+++ src/shorter/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ShortUrl(models.Model):
+    true_url = models.URLField(blank=False)
+    short_url = models.CharField(blank=False)
+    num_use = models.PositiveIntegerField(default=0,blank=True)
