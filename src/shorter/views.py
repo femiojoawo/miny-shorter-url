e8f74f553abbe6e4aa43750ba17bd9338d498c18
femi <femiojoawo@3gmail.com>
2026-01-15 19:21:22 +0000

    creation de l'algo pour enregistrer les liens dans la bd

--- src/shorter/views.py
+++ src/shorter/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from shorter.form import ShorterUrlForm
+from shorter.models import ShortUrl
+from shorter.shorter_algo import shorter_algo
 # Create your views here.
+
+
 def shorturl(request):
     if request.method == 'POST':
         form = ShorterUrlForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
-            
+            short = ShortUrl.objects.create(true_url=form.cleaned_data["url"], short_url= shorter_algo(form.cleaned_data["url"]))
+            short.save()
     else:
         form = ShorterUrlForm()
     context = {"form":form}
