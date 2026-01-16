087e1da63443c7b1337550895454beccfd29b6f3
femi <femiojoawo@3gmail.com>
2026-01-16 09:25:50 +0000

    mise a jour de urls.py et upgrade des short url

--- src/miny/urls.py
+++ src/miny/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('shorter.urls'))
+    path('short',include('shorter.urls'))
     
 ]
 
--- src/shorter/views.py
+++ src/shorter/views.py
@@ -3,20 +3,28 @@ from django.shortcuts import get_object_or_404, render
 from shorter.form import ShorterUrlForm
 from shorter.models import ShortUrl
 from shorter.shorter_algo import shorter_algo
+from django.urls import reverse
 # Create your views here.
 
 
 def shorturl(request):
+    context = {}
     if request.method == 'POST':
         form = ShorterUrlForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
+            try:
+                short = ShortUrl.objects.get(true_url=form.cleaned_data["url"])
+                context['info'] = "l'url existe deja"
+                context["reverse_url"] = f"{reverse('index-short')}/{short.short_suffix}"
+            except ShortUrl.DoesNotExist:
+                created = ShortUrl.objects.create(true_url=form.cleaned_data["url"], short_suffix=shorter_algo(form.cleaned_data["url"]))
+                created.save()
+                context["reverse_url"] = f"{reverse('index-short')}/{ShortUrl.objects.get(true_url=form.cleaned_data["url"]).short_suffix}"
             
-            short = ShortUrl.objects.get_or_create(true_url=form.cleaned_data["url"], short_suffix= shorter_algo(form.cleaned_data["url"]))
-            short.save()
     else:
         form = ShorterUrlForm()
-    context = {"form":form}
+    context['form'] = form
     return render(request,'shorter/index.html',context)
 
 def reverse_url(request,url):
