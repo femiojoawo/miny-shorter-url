4383a7c7be929dee49e19d5175f4dbff55baa11b
femi <femiojoawo@3gmail.com>
2026-01-19 01:20:19 +0000

    suppression des fichiers pour tailwind , localisation a francais de l'app

--- src/miny/settings.py
+++ src/miny/settings.py
@@ -38,9 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shorter',
-     # other Django apps
-    "tailwind",
-    'theme',
 ]
 
 if DEBUG:
@@ -116,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
@@ -130,4 +127,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-TAILWIND_APP_NAME = 'theme'
+
